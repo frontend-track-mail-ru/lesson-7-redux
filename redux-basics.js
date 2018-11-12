386f8b1513083a2543d45507b538eb91f3a526c2
Oleg Klimakov <o.klimakov@corp.mail.ru>
2018-11-12 14:49:17 +0300

    add reducer

--- redux-basics.js
+++ redux-basics.js
@@ -6,12 +6,29 @@ const initialState = {
 };
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if(action.type === 'INC_COUNTER') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if(action.type === 'ADD_COUNTER') {
+    return {
+      counter: state.counter + action.value,
+    };
+  }
   return state;
 };
 
 // Store
 const store = createStore(rootReducer);
-console.log('state ->', store.getState());
+console.log('state before->', store.getState());
 // Dispatching action
-
+store.dispatch({
+  type: 'INC_COUNTER',
+});
+store.dispatch({
+  type: 'ADD_COUNTER',
+  value: 5
+});
+console.log('state after->', store.getState());
 // Subscription
